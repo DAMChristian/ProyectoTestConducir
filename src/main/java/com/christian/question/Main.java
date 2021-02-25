23f57fbd786d98163d7a283c4efb0978b695977d
DAMChristian <56116351+DAMChristian@users.noreply.github.com>
2021-02-25 18:59:03 +0100

    Renombrado package

--- src/main/java/com/christian/question/Main.java
+++ src/main/java/com/christian/question/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.question;
+package com.christian.question;
 
 import com.formdev.flatlaf.FlatDarkLaf;
 import com.fasterxml.jackson.databind.ObjectMapper;
